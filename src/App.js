436a54cbf324c9de76f9fab60cad0b6be9a2bad5
Aviroop Jana <aviroopjana@gmail.com>
2023-04-16 20:46:27 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
 import './App.css';
+import CoinPage from './pages/CoinPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path='/' Component={HomePage} exact/>
+        <Route path='coins/:id' Component={CoinPage}/>
+      </div>
+    </BrowserRouter>
   );
 }
 
