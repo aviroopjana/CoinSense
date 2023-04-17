50f8aeefeb1ea6ba34b98ec5785d11bde228d5f8
Aviroop Jana <aviroopjana@gmail.com>
2023-04-17 23:59:40 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import './App.css';
 import CoinPage from './pages/CoinPage';
+import { makeStyles } from 'tss-react/mui';
 
-function App() {
+const useStyles = makeStyles() (()=> {
+  return{
+    App: {
+      backgroundColor: '#14161A',
+      color: 'white',
+      minHeight: '100vh'
+    }
+  }
+});
+
+function App() {  
+  const { classes } = useStyles();
+
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
+        <Routes>
         <Route path='/' Component={HomePage} exact/>
-        <Route path='coins/:id' Component={CoinPage}/>
+        <Route path='/coins/:id' Component={CoinPage}/>
+        </Routes>
       </div>
     </BrowserRouter>
   );
