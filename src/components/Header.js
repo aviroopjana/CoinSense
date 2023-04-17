91a61086f8efff90da9e4e58a3649e4d5bdf663f
Aviroop Jana <aviroopjana@gmail.com>
2023-04-18 00:00:16 +0530

    updated Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from '@mui/material';
+import makeStyles from 'tss-react';
 
-const Header = () => {
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+
   return (
-    <div>Header</div>
+    <AppBar color='transparent' position='static'>
+      <Container>
+        <Toolbar>
+          <Typography className={classes.title}>
+            CoinSense
+          </Typography>
+
+          <Select>
+            <MenuItem value ='USD'>USD</MenuItem>
+            <MenuItem value ='INR' >INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 
-export default Header
+export default Header;
