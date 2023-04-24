60015d2a96216b287662d3dbc82940c65b53cb2a
Aviroop Jana <aviroopjana@gmail.com>
2023-04-24 12:54:51 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 import CoinPage from "./pages/CoinPage";
+
 import { makeStyles } from "tss-react/mui";
 
 const useStyles = makeStyles()(() => {
